d60f5a7d991c21ceead2d7acc96b2d0d578d3245
Tim Quigg <timjquigg@gmail.com>
2022-11-24 14:25:50 -0700

    Updated db connection info

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,8 @@ const dbParams = {
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: true
 };
 
 const db = new Pool(dbParams);
