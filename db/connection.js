cc525b765bb1654ea567314c7f249a0a8152a9a9
Tim Quigg <timjquigg@gmail.com>
2022-11-24 14:28:35 -0700

    Update db connection info for Heroku Postgres

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ const dbParams = {
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  ssl: true
+  ssl: {rejectUnauthorized: false}
 };
 
 const db = new Pool(dbParams);
